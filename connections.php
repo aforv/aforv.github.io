28163846745246e13d13cf100bb3587d4b5327ff
weebo <99533260+aforv@users.noreply.github.com>
2024-01-08 01:07:17 +0700

    Delete connections.php

--- connections.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$con = mysqli_connect("sql313.0fees.us","0fe_34443764","45y89h59hnugkirthgy8grh8r","0fe_34443764_posts");
-
-	if (mysqli_connect_errno()) {
-	  echo "Error connecting to the database:" . mysqli_connect_error();
-	  exit();
-	}
-?>
